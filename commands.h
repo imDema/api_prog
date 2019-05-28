a500f2f24cbbb360f788e518d0fe5db51937fc7a
imDema <luca.de.martini@protonmail.com>
2019-05-28 13:45:24 +0200

    Added main functions

---
+++ commands.h
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void addent(char* id_ent)
+{
+
+}
+
+void delent(char* id_ent)
+{
+
+}
+
+void addrel(char* id_orig, char* id_dest, char* id_rel)
+{
+
+}
+
+void delrel(char* id_orig, char* id_dest, char* id_rel)
+{
+
+}
+
+void report()
+{
+
+}
