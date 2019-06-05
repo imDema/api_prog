a3be08c0b44f4406313bfe98a0c5398392cb2a1d
imDema <luca.de.martini@protonmail.com>
2019-06-05 11:12:54 +0200

    rel_item and toplist skeleton

--- structures.h
+++ structures.h
@@ -3,8 +3,9 @@
 
 struct _link_item
 {
-    struct _link_item* next;
+    char* id_link;
     relarray relations;
+    struct _link_item* next;
 };
 typedef struct _link_item* link_item;
 
@@ -14,6 +15,7 @@ void ht_link_free(void* entry)
     if(item->next != NULL)
         ht_link_free(item->next);
     relarray_free(item->relations);
+    free(item->id_link);
     free(item);
 }
 
@@ -33,16 +35,45 @@ void ll_free(ll_node_links item)
 
 struct _ent_item
 {
+    char* id_ent;
     ll_node_links links;
     countarray relcounts;
+    struct _ent_item* next;
 };
 typedef struct _ent_item* ent_item;
 
 void ht_ent_free(void* entry)
 {
     ent_item item = (ent_item) entry;
+    if(item->next != NULL) ht_ent_free(item->next);
     countarray_free(item->relcounts);
+    free(item->id_ent);
     ll_free(item->links);
 }
 
-void ht_rel_free(void* entry);
+struct _toplist
+{
+    struct _toplist* next;
+    int value;
+    int size;
+    int count;
+    ent_item item;
+};
+typedef struct _toplist* toplist;
+
+struct _rel_item
+{
+    char* id_rel;
+    int index;
+    int active_count;
+    toplist top;
+};
+typedef struct _rel_item* rel_item;
+
+void ht_rel_free(void* entry)
+{
+    rel_item item = (rel_item) entry;
+    //TODO free toplsìist
+    free(item->id_rel);
+    free(item);
+}
